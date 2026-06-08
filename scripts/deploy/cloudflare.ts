f9b126f0eb5173b515e2bcaa1ff4a3b6d10e7c58
Robin.Wang <3Jg$94sbQ@q1>
2026-06-08 15:23:37 +0800

    build(project): update default branch

--- scripts/deploy/cloudflare.ts
+++ scripts/deploy/cloudflare.ts
@@ -27,7 +27,7 @@ export const createPages = async () => {
   const project = await client.pages.projects.create({
     account_id: CF_ACCOUNT_ID,
     name: PROJECT_NAME,
-    production_branch: "main",
+    production_branch: "master",
   });
 
   if (CUSTOM_DOMAIN) {
